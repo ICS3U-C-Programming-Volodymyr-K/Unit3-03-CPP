9f6bddd7a9004bb99ba06594a074bf483aafe7f7
Volodymyr Kryzhanovskyi <volodymyr.kryzhnovskyi@stu.ocsb.ca>
2025-03-24 04:40:57 +0000

    Update.

--- randNumGuess.cpp
+++ randNumGuess.cpp
@@ -3,22 +3,22 @@
 // Created on: 03-23-2025
 // This program makes a gambling number guess
 
-#include <iostream>  
+#include <iostream>
 #include <cstdlib>
 
-int main(){
+int main() {
     // gets number from user and declares the variable for it
     int userNumber;
     std::cout << "Insert random number from 0 to 9";
     std::cin >> userNumber;
     // generates random number
-    int randomNumber = rand() % 10;
-    //compares if numbers are the same, if they are displays win message
+    int randomNumber = rand_r() % 10;
+    // compares if numbers are the same, if they are displays win message
     if (userNumber == randomNumber) {
         std::cout << "You've guessed correctly. \n";
     }
-    // if they are not the same then generates lose message and correct number
+// if they are not the same then generates lose message and correct number
     else {
-        std::cout << "Your guess is incorrect, the number is " << randomNumber << std::endl;
+        std::cout << "Your guess is incorrect, the number is " << randomNumber
+        << std::endl;
     }
-}
